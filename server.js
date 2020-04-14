74bc86e7c404af177b8743dcd1ba19a2f23f4205
Bushra-B <bushraxbil@gmail.com>
2020-04-14 03:13:07 +0300

    refactore handling routes + render search results + css edits

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 'use strict';
 
-//Load enviroument variables
+//Load enviroument variable
 require('dotenv').config();
 
-//Server dependencies
+//Server Dependencies
 const express = require('express');
 const superagent = require('superagent');
 
@@ -11,16 +11,14 @@ const superagent = require('superagent');
 const app = express();
 const PORT = process.env.PORT || 3030;
 
-//Specify a directory for static resourses
+//Specigy static resourses directory
 app.use(express.static('./public'));
 
-//Add POST form data to request.body
+//Express middleware - Add POST form data to request.body
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 //Set the view engine
-// app.set('views', './views/pages');
-// app.set('views', './views/pages/searches');
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
@@ -29,59 +27,52 @@ app.get('/', (request, response) => {
   response.status(200).send('Working');
 });
 
-//Temp route for testing
-app.get('/test',(request,response) => {
+//Temo route for testing
+app.get('/test', (request, response) => {
   response.render('pages/index');
 });
 
-//Search page route
-app.get('/searches/new', (request, response) => {
-  response.render('pages/searches/new');
-});
+//Routes
+app.get('/searches/new', searchHandler); //Search page
+app.post('/searches', getData); //Form POST request
 
-//POST rquest route
-app.post('/views/pages/searches', (request, response) => {
+//Handling search page route
+function searchHandler(request, response) {
+  response.render('pages/searches/new');
+}
+
+//Handling POST request route
+function getData(request, response) {
   let q = request.body.q;
   let searchBy = request.body.search;
   console.log('qs :', q);
   console.log('s :', searchBy);
   console.log('POST request: ', request.body);
   let dataArr;
+  let url = `https://www.googleapis.com/books/v1/volumes?q=`;
   if (searchBy === 'title') {
-    let url = `https://www.googleapis.com/books/v1/volumes?q=${q}+intitle:${q}`;
-    superagent.get(url)
-      .then(searchResults => {
-        let data = searchResults.body.items;
-        dataArr = data.map(value => {
-          new Book(value);
-        });
-        console.log('Results: ', dataArr);
-        response.render('Results: ', dataArr);
-
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  } else if (searchBy === 'author') {
-    let url = `https://www.googleapis.com/books/v1/volumes?q=${q}+inauthor:${q}`;
-    superagent.get(url)
-      .then(searchResults => {
-        let data = searchResults.body.items;
-        dataArr = data.map(value => {
-          new Book(value);
-        });
-        console.log('Results: ', dataArr);
-        response.render('Results: ', dataArr);
-
-      })
-      .catch(error => {
-        console.error(error);
-      });
+    url += `${q}+intitle:${q}`;
   }
+  else if (searchBy === 'author') {
+    url += `${q}+inauthor:${q}`;
+  }
+  superagent.get(url)
+    .then(searchResults => {
+      let data = searchResults.body.items;
+      dataArr = data.map(value => {
+        let book = new Book(value);
+        return book;
+      });
+      console.log('Results: ', dataArr);
+      response.render('pages/searches/show', {dataArr: dataArr});
+    })
+    .catch(error => {
+      console.error(error);
+    //   response.render('pages/error');
+    });
+}
 
-});
-
-let thumbnailImg = './book-icon-156.png';
+let thumbnailImg = 'https://www.freeiconspng.com/uploads/book-icon-black-good-galleries--24.jpg';
 const regex = /http/gi;
 
 //C.F
@@ -96,9 +87,4 @@ function Book(bookData) {
 app.listen(PORT, () => {
   console.log('Listening on prot:', PORT);
 });
-
-
-
-
-
 
